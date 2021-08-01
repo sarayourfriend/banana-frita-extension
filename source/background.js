d54160fa4a888b3f2d5afc8622aaa90607865bdf
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-08-01 09:52:08 -0700

    Make it an extension

--- source/background.js
+++ source/background.js
@@ -1,2 +1,12 @@
-// eslint-disable-next-line import/no-unassigned-import
-import './options-storage.js';
+import optionsStorage from './options-storage.js';
+import browser from 'webextension-polyfill';
+
+browser.runtime.onMessage.addListener(async (message, sender) => {
+	if (message.popupOpen) {
+		const [tab] = await browser.tabs.query({ active: true });
+		const { api } = await optionsStorage.getAll();
+		const url = `${api}/api/bookmark?url=${tab.url}`;
+		console.log('URL', url);
+		fetch(url).then(console.log).catch(console.error);
+	}
+});
--- source/options-storage.js
+++ source/options-storage.js
@@ -2,9 +2,7 @@ import OptionsSync from 'webext-options-sync';
 
 export default new OptionsSync({
 	defaults: {
-		colorRed: 244,
-		colorGreen: 67,
-		colorBlue: 54
+		api: 'http://localhost:3000',
 	},
 	migrations: [
 		OptionsSync.migrations.removeUnused
